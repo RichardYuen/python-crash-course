9b330d5099dcad06756907397b6a054856709ca5
not-null-x11 <nhatquang97@gmail.com>
2018-07-18 03:46:40 -0400

    Project code for Python Crash Course book

---
+++ Project 1 Alien Invasion/alien_invasion.py
@@ -0,0 +1,46 @@
+import pygame
+
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
+from pygame.sprite import Group
+
+def run_game():
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+
+	#Make a ship
+	ship = Ship(ai_settings, screen)
+
+	#Make a group to store bullets in.
+	bullets = Group()
+	
+	#Make a group to store aliens in.
+	aliens = Group()
+
+	#Create the fleet of aliens.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+
+	#Start the main loop for the game
+	while True:
+		gf.check_events(ai_settings, screen, ship, bullets)
+		ship.update()
+		bullets.update()
+
+		#Get rid of bullets that have disappeared.
+		gf.update_bullets(aliens ,bullets)
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		screen.fill(ai_settings.bg_color)
+		ship.blitme()
+
+		#Make the most recently drawn screen visible
+		pygame.display.flip()
+
+
+run_game()
+
